e49cabf1c67fa65316856e6396020985d9305429
Miguel Ortiz <tecnicohernandez2018@gmail.com>
2021-06-30 15:54:03 -0500

    Excepciones Snapshot

--- __test__/snapshot.test.js
+++ __test__/snapshot.test.js
@@ -7,4 +7,22 @@ describe('Es hora de las instantaneas', () => {
     test('Snapshot', () =>{
         expect(getCharacter(rick)).toMatchSnapshot();
     });
+
+    test('Siempre falllara la instantanea', () => {
+        const user = {
+            createAt: new Date(),
+            id: Math.floor(Math.random() * 20),
+        }
+        expect(user).toMatchSnapshot();
+    });
+
+    test('Tenemos una excepción dentro del codigo', () => {
+        const user = {
+            id: Math.floor(Math.random() * 20),
+            name: "Laura Guzman Martinez"
+        }
+        expect(user).toMatchSnapshot({
+            id: expect.any(Number)
+        })
+    });
 });
